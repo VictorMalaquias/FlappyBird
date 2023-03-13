63f5f6fac81e9f763d5a6f7a76cf981df933e3e9
Victor Hugo G. Malaquias <victormalaquias@outlook.com.br>
2023-03-13 09:21:32 -0300

    flappy

---
+++ flappy_byrd/lib/coverscreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyCoverScreen extends StatelessWidget {
+  final bool gameHasStarted;
+
+  const MyCoverScreen({super.key, required this.gameHasStarted});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: const Alignment(0, -0.5),
+      child: Text(
+        gameHasStarted ? '' : 'T A P  T O  P L A Y',
+        style: const TextStyle(color: Colors.white, fontSize: 25),
+      ),
+    );
+  }
+}
